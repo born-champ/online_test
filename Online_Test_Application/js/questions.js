a65bc947c782a15a21bfd73678cd33f613560673
Ashutosh Dubey <ashutosh.dubey@COMVIVA.COM>
2021-11-13 20:32:03 +0530

    done

---
+++ Online_Test_Application/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers in JSON format
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    
+    {
+    numb: 3,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is making the Web standards?",
+    answer: "The World Wide Web Consortium",
+    options: [
+      "The World Wide Web Consortium",
+      "Google",
+      "Microsoft",
+      "Mozilla"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Choose the correct HTML element for the largest heading:",
+    answer: "h1",
+    options: [
+      "h1",
+      "h6",
+      "head",
+      "heading"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Choose the correct HTML element to define important text",
+    answer: "strong",
+    options: [
+      "strong",
+      "b",
+      "important",
+      "i"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which character is used to indicate an end tag?",
+    answer: "/",
+    options: [
+      "/",
+      "<",
+      "*",
+      "^"
+    ]
+  },
+  {
+    numb: 9,
+    question: "How do you write `Hello World` in an alert box?",
+    answer: "alert(`Hello World`);",
+    options: [
+      "alert(`Hello World`);",
+      "msgBox(`Hello World`);",
+      "msg(`Hello World`);",
+      "alertBox(`Hello World`);"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How to write an IF statement in JavaScript?",
+    answer: "if (i == 5)",
+    options: [
+      "if (i == 5)",
+      "if i == 5 then",
+      "if i = 5 then",
+      "if i = 5"
+    ]
+  },
+];
